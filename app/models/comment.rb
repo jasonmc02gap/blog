114ee318b81216fe5672c344d97540958441715a
Fernanda <fermarioro@gmail.com>
2015-11-10 13:44:26 -0600

    Removed lists as they are throwing a merge error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,13 +7,4 @@ class Comment
 
   view :all, :key => :_id, :properties => [:_id, :_rev, :created_at, :updated_at], :type => :properties
 
-  list :json_array, <<-JS
-    function (head, req) {
-      var row, objects = [];
-      while (row = getRow()) {
-        objects.push(JSON.stringify(row.value));
-      };
-      send("[" + objects.join(",") + "]");
-    }
-  JS
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,4 @@ class User
 
   view :all, :key => :_id, :properties => [:_id, :_rev, :created_at, :updated_at], :type => :properties
 
-  list :json_array, <<-JS
-    function (head, req) {
-      var row, objects = [];
-      while (row = getRow()) {
-        objects.push(JSON.stringify(row.value));
-      };
-      send("[" + objects.join(",") + "]");
-    }
-  JS
-
 end
