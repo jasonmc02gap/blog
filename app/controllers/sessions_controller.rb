e20c778c151b5e5ee345c5b5b5d379740a4d5e3a
Fernanda <fermarioro@gmail.com>
2015-11-12 13:24:06 -0600

    Log in session missing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
+      session[:slug] = user.slug
       redirect_to root_url, notice: "Logged in"
     else
        flash.now.alert = "Invalid email or password"
